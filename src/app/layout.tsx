70b23c7da0132f6c7b7a99606c30da3a6751c712
Yaroslav-Chertov <yachertov@yandex.ru>
2026-04-02 23:48:34 +0300

    fix img and video

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,31 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+
+        {/* Yandex.Metrika */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108256546','ym');
+        ym(108256546,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+      `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/108256546"
+              style={{ position: "absolute", left: -9999 }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
